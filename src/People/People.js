015bfdf71faefbfeddd5a4a1bef8ea8a17d9f304
yennguyenthihoangvn <yennguyenthihoangvn@gmail.com>
2019-06-12 02:11:39 +0200

    Update people.js

--- src/People/People.js
+++ src/People/People.js
@@ -1,86 +1,106 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
+
 
 class People extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      starship: null,
+      filteredPeople: [],
+      people: []
     };
+    this.searchPeopleByName = this.searchPeopleByName.bind(this);
   }
 
-  async componentDidMount() {
-    const { match: { params } } = this.props;
-    axios.get('https://swapi.co/api/starships/12/').then(res => {
-      const starship = res.data;
-      console.log("Starship: " + starship);
+  componentDidMount() {
+    //Get People from backend
+    axios.get('https://swapi.co/api/people/').then(res => {
+      const people = res.data.results;
+      console.log("People " + people);
+      const filteredPeople = people;
       this.setState({
-        starship,
+        people, 
+        filteredPeople,
       });
     })
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      filteredPeople: nextProps.items
+    });
+  }
+
+  searchPeopleByName(e){
+    // Variable to hold the original version of the list
+    let currentList = [];
+    // Variable to hold the filtered list before putting into state
+    let newList = [];
+
+    // If the search bar isn't empty
+    if (e.target.value !== "") {
+      // Assign the original list to currentList
+      currentList = this.state.people;
+
+      // Use .filter() to determine which items should be displayed
+      // based on the search terms
+      newList = currentList.filter(item => {
+        // change current item to lowercase
+        const lc = item.name.toLowerCase();
+        // change search term to lowercase
+        const filter = e.target.value.toLowerCase();
+        // check to see if the current list item includes the search term
+        // If it does, it will be added to newList. Using lowercase eliminates
+        // issues with capitalization in search terms and search content
+        return lc.includes(filter);
+      });
+    } else {
+      // If the search bar is empty, set newList to original task list
+      newList = this.state.people;
+    }
+    // Set the filtered state based on what our rules added to newList
+    this.setState({
+      filteredPeople: newList
+    });
+  }
+
   render() {
-    const {starship} = this.state;
-    if (starship === null) return <p>Loading ...</p>;
     return (
       <div className="container">
-          <div className="jumbotron col-12">
-            <h4 className="display-5 font-weight-bold offset-md-4">Starship Information - {starship.name}</h4>
-            <hr className="my-4" />
-            <div className="row col-sm-12 col-md-12 col-lg-12">
-              <div className="col-sm-12 col-md-4 col-lg-4">
-                <p className="lead"><b>Model: </b>{starship.model}</p>
-                <p className="lead"><b>Manufacturer: </b>{starship.manufacturer}</p>
-                <p className="lead"><b>Const in Credits: </b>{starship.cost_in_credits}</p>
-                <p className="lead"><b>Length: </b>{starship.length}</p>
-                <p className="lead"><b>Max aatmosphering speed: </b>{starship.max_atmosphering_speed}</p>
-              </div> 
-              <div className="col-sm-12 col-md-4 col-lg-4">
-                <p className="lead"><b>Crew: </b>{starship.crew}</p>
-                <p className="lead"><b>Passengers: </b>{starship.passengers}</p>
-                <p className="lead"><b>Cargo capacity: </b>{starship.cargo_capacity}</p>
-                <p className="lead"><b>Consumables: </b>{starship.consumables}</p>
-                <p className="lead"><b>Hyperdrive rating: </b>{starship.hyperdrive_rating}</p>
-              </div> 
-              <div className="col-sm-12 col-md-4 col-lg-4">
-                <p className="lead"><b>MGLT: </b>{starship.MGLT}</p>
-                <p className="lead"><b>Starship class: </b>{starship.starship_class}</p>
-                <p className="lead"><b>Cargo capacity: </b>{starship.cargo_capacity}</p>
-                <p className="lead"><b>Created date: </b>{starship.created}</p>
-                <p className="lead"><b>Edited date: </b>{starship.edit}</p>
+        <div className="topnav">
+          <div className="search-container">
+            <form action="/action_page.php">
+              <input type="text" onChange={this.searchPeopleByName} placeholder="Search by name..." name="search"/>
+              <button type="submit"><FontAwesomeIcon icon="search" /></button>
+            </form>
+          </div>
+        </div>
+
+        <div className="row">
+          {this.state.filteredPeople === null && <p>Loading People...</p>}
+          {
+            this.state.filteredPeople && this.state.filteredPeople.map(person => (
+              <div key={person.url.split('/')[5]} className="col-sm-12 col-md-4 col-lg-3">
+                <Link to={`/person/${person.url.split('/')[5]}`}>
+                  <div className="card text-info text-white bg-white mb-3">
+                    <div className="card-body">
+                      <h5 className="card-title">{person.name}</h5>
+                      <p className="card-text"><b>Gender: </b>{person.gender}</p>
+                      <p className="card-text"><b>Birth Year: </b>{person.birth_year}</p>
+                      <p className="card-text"><b>Height: </b>{person.height} cm</p>
+                      <p className="card-text"><b>Mass: </b>{person.mass}</p>
+                      <p className="card-text"><b>Hair Color: </b>{person.hair_color}</p>
+                      <p className="card-text"><b>Skin Color: </b>{person.skin_color}</p>
+                      <p className="card-text"><b>Eye color: </b>{person.eye_color}</p>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </div>
-            <hr className="my-4" />
-            <div className="row col-sm-12 col-md-12 col-lg-12">
-              <div className="col-sm-12 col-md-6 col-lg-6">
-                <p className="font-weight-bold"><b>Pilots:</b></p>
-                {this.state.starship === null && <p>Loading Starship...</p>}
-                  {
-                    this.state.starship.pilots && this.state.starship.pilots.map(pilot => (
-                      <div key={pilot} className="col-sm-12 col-md-4 col-lg-3">
-                        <Link to={`${pilot}`}>
-                          <p className="card-text">{pilot}</p>
-                        </Link>
-                      </div>
-                    ))
-                  }
-              </div> 
-              <div className="col-sm-12 col-md-6 col-lg-6">
-                <p className=""><b>Films:</b></p>
-                {this.state.starship === null && <p>Loading Starship...</p>}
-                  {
-                    this.state.starship.films && this.state.starship.films.map(film => (
-                      <div key={film} className="col-sm-12 col-md-4 col-lg-3">
-                        <Link to={`${film}`}>
-                          <p className="card-text">{film}</p>
-                        </Link>
-                      </div>
-                    ))
-                  }
-              </div> 
-            </div>
+            ))
+          }
         </div>
       </div>
     )
